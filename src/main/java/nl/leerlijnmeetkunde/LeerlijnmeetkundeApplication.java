b33f8eac2b3fc22783f0ebff22b91a3adf6d3592
RemarkableGuy <alvaro.schipper@hotmail.com>
2017-03-04 15:31:23 +0100

    Initial commit

---
+++ src/main/java/nl/leerlijnmeetkunde/LeerlijnmeetkundeApplication.java
@@ -0,0 +1,12 @@
+package nl.leerlijnmeetkunde;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeerlijnmeetkundeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeerlijnmeetkundeApplication.class, args);
+	}
+}
